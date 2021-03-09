5ef96b3cae735b1b0bfb5fea5117fb8368398d93
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2021-03-09 11:16:48 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const devMode = process.env.DevMode === 'true'
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['prettier', 'prettier/standard', 'plugin:vue/recommended'],
+  plugins: ['vue', 'prettier'],
+  rules: devMode
+    ? {}
+    : {
+        'prettier/prettier': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/html-self-closing': 'off',
+      },
+}
